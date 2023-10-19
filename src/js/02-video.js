cbeb6c65ec4afd4c5a5a4bcedf4240421278cd0c
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2023-10-19 18:22:34 +0100

    change - 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,26 @@
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe, {
+    loop: true,
+    fullscreen: true,
+    quality: '1080p',
+  });
 
 player.on('timeupdate', throttle(function(event) {
-    console.log('Поточний час:', event.seconds);
-    localStorage.setItem('videoplayer-current-time', event.seconds);
+  console.log('Поточний час:', event.seconds);
+  localStorage.setItem('videoplayer-current-time', event.seconds);
 }, 1000));
 
 document.addEventListener('DOMContentLoaded', function() {
-    const savedTime = localStorage.getItem('videoplayer-current-time');
-    if (savedTime) {
-        player.ready().then(function() {
-            player.setCurrentTime(savedTime);
-        });
-    }
-    console.log('Збережений час:', savedTime);
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  
+  if (savedTime) {
+    player.ready().then(function() {
+      player.setCurrentTime(savedTime);
+    });
+  }
+  
+  console.log('Збережений час:', savedTime);
 });
